1690e7f043870acc3d7d7c928e6bd39f0d32311f
luisitohc <luishernandezopsu2019@gmail.com>
2026-09-22 12:58:06 -0400

    Primer commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <p className="font-bold text-4xl m-10 text-white bg-emerald-800 rounded-xl p-8">
+      Ivoices Page
+    </p>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <p className="font-bold text-4xl m-10 text-white bg-slate-800 rounded-xl p-8">
+      Dashboard Page
+    </p>
+  );
+}
+
